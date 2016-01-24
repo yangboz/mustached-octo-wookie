2ea0b162f312207824bbf6ad0d98b384e4656a03
yangboz <zhou.yangbo@intellif.com>
2016-01-24 09:19:58 +0800

    ADDED:location/date model binary file and applied the example API

--- src/main/java/info/smartkit/spring_data_solr/controllers/OpenNLPcontroller.java
+++ src/main/java/info/smartkit/spring_data_solr/controllers/OpenNLPcontroller.java
@@ -53,10 +53,10 @@ public class OpenNLPcontroller {
         return new File(getModelDir(), "en-ner-person.bin");
     }
 
-    @RequestMapping(value = "/names", method = RequestMethod.GET)
-    @ApiOperation(httpMethod = "GET", value = "Response a list describing all of book info that is successfully get or not."
+    @RequestMapping(value = "/models/person", method = RequestMethod.GET)
+    @ApiOperation(httpMethod = "GET", value = "Response a list<personModel> describing all of book info that is successfully get or not."
             , notes = "use OpenNLP to identify people by writing a few lines of Java code.")
-    public List<Map> findNames() throws IOException {
+    public List<Map> findByPersonModel() throws IOException {
         List<Map> results = new ArrayList<Map>();
         String[] sentences = {
                 "Former first lady Nancy Reagan was taken to a " +
@@ -85,4 +85,132 @@ public class OpenNLPcontroller {
         return results;
     }
 
+    @RequestMapping(value = "/models/person/location/date", method = RequestMethod.GET)
+    @ApiOperation(httpMethod = "GET", value = "Response a list<person/location/dateModel> describing all of book info that is successfully get or not."
+            , notes = "It also implies that different models can iden- tify names from overlapping sections of text.")
+    public List<Annotation> findByPersonLocationDateModel() throws IOException {
+        String[] sentences = {
+                "Former first lady Nancy Reagan was taken to a " +
+                        "suburban Los Angeles " +
+                        "hospital as a precaution Sunday after a fall at " +
+                        "her home, an " +
+                        "aide said. ",
+                "The 86-year-old Reagan will remain overnight for " +
+                        "observation at a hospital in Santa Monica, California, " +
+                        "said Joanne " +
+                        "Drake, chief of staff for the Reagan Foundation."};
+        NameFinderME[] finders = new NameFinderME[3];
+        String[] names = {"person", "location", "date"};
+        for (int mi = 0; mi < names.length; mi++) {
+            finders[mi] = new NameFinderME(new TokenNameFinderModel(
+                    new FileInputStream(
+                            new File(getModelDir(), "en-ner-" + names[mi] + ".bin")
+                    )));
+        }
+        //
+        Tokenizer tokenizer = SimpleTokenizer.INSTANCE;
+        for (int si = 0; si < sentences.length; si++) {
+            List<Annotation> allAnnotations = new ArrayList<Annotation>();
+            String[] tokens = tokenizer.tokenize(sentences[si]);
+//            Initialize new model for identifying people, locations, and dates based on the binary compressed model in the files en-ner- person.bin, en-ner- location.bin, en-ner-date.bin.
+//                    Obtain reference to a tokenizer to split sentence into individual words and symbols.
+//            Split sentence into array of tokens.
+//            Iterate over each name finder (person, location, date).
+//                    Get probabilities with associated matches.
+            for (int fi = 0; fi < finders.length; fi++) {
+                Span[] spans = finders[fi].find(tokens);
+                double[] probs = finders[fi].probs(spans);
+                for (int ni = 0; ni < spans.length; ni++) {
+//                Identify names in sentence and return token - based offsets.
+                    allAnnotations.add(
+                            new Annotation(names[fi], spans[ni], probs[ni])
+                    );
+                }
+            }
+            removeConflicts(allAnnotations);
+            return allAnnotations;
+        }
+        return null;
+    }
+
+    private void removeConflicts(List<Annotation> allAnnotations) {
+        java.util.Collections.sort(allAnnotations);
+        List<Annotation> stack = new ArrayList<Annotation>();
+        stack.add(allAnnotations.get(0));
+        for (int ai = 1; ai < allAnnotations.size(); ai++) {
+            Annotation curr = (Annotation) allAnnotations.get(ai);
+            boolean deleteCurr = false;
+            for (int ki = stack.size() - 1; ki >= 0; ki--) {
+                Annotation prev = (Annotation) stack.get(ki);
+                if (prev.getSpan().equals(curr.getSpan())) {
+                    if (prev.getProb() > curr.getProb()) {
+                        deleteCurr = true;
+                        break;
+                    } else {
+                        allAnnotations.remove(stack.remove(ki));
+                        ai--;
+                    }
+                } else if (prev.getSpan().intersects(curr.getSpan())) {
+                    if (prev.getProb() > curr.getProb()) {
+                        deleteCurr = true;
+                        break;
+                    } else {
+                        allAnnotations.remove(stack.remove(ki));
+                        ai--;
+                    }
+                } else if (prev.getSpan().contains(curr.getSpan())) {
+                    break;
+                } else {
+                    stack.remove(ki);
+                }
+            }
+            if (deleteCurr) {
+                allAnnotations.remove(ai);
+                ai--;
+                deleteCurr = false;
+            } else {
+                stack.add(curr);
+            }
+        }
+    }
+
+
+    class Annotation implements Comparable<Annotation> {
+        private Span span;
+        private String type;
+        private double prob;
+
+        public Annotation(String type, Span span, double prob) {
+            this.span = span;
+            this.type = type;
+            this.prob = prob;
+        }
+
+        public Span getSpan() {
+            return span;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public double getProb() {
+            return prob;
+        }
+
+        public int compareTo(Annotation a) {
+            int c = span.compareTo(a.span);
+            if (c == 0) {
+                c = Double.compare(prob, a.prob);
+                if (c == 0) {
+                    c = type.compareTo(a.type);
+                }
+            }
+            return c;
+        }
+
+        public String toString() {
+            return type + " " + span + " " + prob;
+        }
+    }
 }
